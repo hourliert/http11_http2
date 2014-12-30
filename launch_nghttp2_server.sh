0518380ab535446d7fad2376ada0a8fd0ed2241a
Thomas Hourlier <thomas.hourlier@cnode.fr>
2014-12-30 11:28:45 +0100

    added push test

--- launch_nghttp2_server.sh
+++ launch_nghttp2_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd public
-nghttpd -v 3001 ../keys/server.key ../keys/server.crt --push=/Multiplexing.html=/chaton.png
+nghttpd -v 3001 ../keys/server.key ../keys/server.crt --push=/push.html=/images/chaton.png
