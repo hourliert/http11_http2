485b8e70c388de0ec8faa656164cc3b76487333f
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-01-01 20:40:31 +0100

    MINOR : nghttx configuration updated

--- launch_nghttpx_proxy.sh
+++ launch_nghttpx_proxy.sh
@@ -2,5 +2,5 @@
 
 
 #REMEMBER to desactivate ssl in nginx !
-nghttpx -LINFO --backend=127.0.0.1,3000 --frontend=127.0.0.1,8080 keys/proxy.key keys/proxy.crt
+nghttpx -LINFO --backend=127.0.0.1,3000 --frontend=127.0.0.1,3001 keys/proxy.key keys/proxy.crt
 
