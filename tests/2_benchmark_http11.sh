8edc1b41839ab650bac22fdd3adfa55517decdf4
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-01-04 13:19:24 +0100

    added benchmark tests

--- tests/2_benchmark_http11.sh
+++ tests/2_benchmark_http11.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-siege -q -c100 -b -r100 https://localhost:3000/Multiplexing.html
+siege -q -c10 -b -r100 https://localhost:3000/Multiplexing.html
---
+++ tests/5_benchmark_http2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+h2load -n100000 -c100 -m1 -t1 https://localhost:3001/Multiplexing.html
